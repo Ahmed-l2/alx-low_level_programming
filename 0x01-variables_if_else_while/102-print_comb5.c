20705c2bb2d4d59bc077899e5d223d167f660d4c
Ahmed <ahmed_l2@yahoo.com>
2023-09-08 17:55:19 -0700

    .

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there*/
+
+/**
+ * main - Entry point
+ * sizeof: provides byte and size of variable
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int firstDigit = 0, secondDigit;
+
+	while (firstDigit <= 99)
+	{
+		secondDigit = firstDigit;
+		while (secondDigit <= 99)
+		{
+			if (secondDigit != firstDigit)
+			{
+				putchar((firstDigit / 10) + 48);
+				putchar((firstDigit % 10) + 48);
+				putchar(' ');
+				putchar((secondDigit / 10) + 48);
+				putchar((secondDigit % 10) + 48);
+
+				if (firstDigit != 98 || secondDigit != 98)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			secondDigit++;
+		}
+		firstDigit++;
+	}
+	putchar('\n');
+
+	return (0);
+}
