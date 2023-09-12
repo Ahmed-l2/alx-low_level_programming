bb892ca8cf69d8a83e019a889cace031af7935ee
Ahmed <ahmed_l2@yahoo.com>
2023-09-12 12:15:43 -0700

    a function that prints 10 times the alphabet, in lowercase, followed by a new line.

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* main -  Entry point
+*
+* Description : prints _putc har using putchar prototype
+*
+* Return: Always 0 (Sucess)#include "main.h"
+*/
+
+int main(void)
+{
+
+	print_alphabet_x10();
+	return (0);
+
+}
