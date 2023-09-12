84ee0dc18d07eeb598ccc02b178015811447f177
Ahmed <ahmed_l2@yahoo.com>
2023-09-12 15:09:02 -0700

    .

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,16 +8,18 @@
  */
 
 
-int _islower(int c)
+int _isalpha(int c)
 {
-        if (c >= 65 && c <= 90 && c >= 97 && c <= 122)
-        {
-                return (1);
-        }
-
-        else
-        {
-                return (0);
-        }
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
-
