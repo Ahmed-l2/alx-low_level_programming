40ed2de7401866fe9ad1c36a1351d86f8140f08f
Ahmed <ahmed_l2@yahoo.com>
2023-09-13 21:26:18 -0700

    .

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _islower - function prototype
+ *
+ * @c: checks input of function
+ *
+ * Return: 1 if uppercase otherwise 0
+ */
+
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+}
+
