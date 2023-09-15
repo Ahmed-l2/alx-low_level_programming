9f4d1adf82198556874069e6039df6ff586f27e9
Ahmed <ahmed_l2@yahoo.com>
2023-09-14 17:27:06 -0700

    a function that prints an integer.

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -14,30 +14,15 @@ void print_number(int n)
 	if (n < 0)
 	{
 		_putchar('-');
-		_putchar((n * -1) / 10 + 48);
-		_putchar((n * -1) % 10 + 48);
+		print_number(-n);
 	}
-	else if (n <= 9)
+	else if (n >= 10)
+	{
+		print_number(n / 10);
+		_putchar((n % 10) + 48);
+	}
+	else if ( n <= 9)
 	{
 		_putchar(n + 48);
-	}
-	else if (n <= 99)
-	{
-		_putchar((n / 10) + 48);
-		_putchar((n % 10) + 48);
-
-	}
-	else if (n <= 999)
-	{
-		_putchar((n / 100) + 48);
-		_putchar((n % 100) / 10 + 48);
-		_putchar((n % 100) % 10 + 48);
-	}
-	else if (n <= 9999)
-	{
-		_putchar((n / 1000) + 48);
-		_putchar((n / 100) % 10 + 48);
-		_putchar((n % 100) / 10 + 48);
-		_putchar((n % 100) % 10 + 48);
 	}
 }
