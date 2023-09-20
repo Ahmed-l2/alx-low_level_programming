fb08203d2872dc44be8648524f92be4da9ad250d
Ahmed <ahmed_l2@yahoo.com>
2023-09-20 11:16:12 -0700

    a function that concatenates two strings.

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strncat - a function that concatenates two strings
+ *
+ * @dest: where the src will append to
+ *
+ * @src: string to be appended
+ *
+ *@n: number of bytes to be concatenated
+ *
+ * Return: both strings concatenated
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int l = 0, i = 0;
+
+	while (dest[l])
+	{
+		l++;
+	}
+
+	while (src[i] < n && src[i] != '\0')
+	{
+		dest[l] = src[i];
+		i++;
+		l++;
+	}
+
+	return (dest);
+}
