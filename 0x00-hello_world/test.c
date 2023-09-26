bf92cc8d8bd6f84097c658500446e99087f2649a
Ahmed <ahmed_l2@yahoo.com>
2023-09-26 04:33:39 -0700

    .

---
+++ 0x00-hello_world/test.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * sizeof: provides byte and size of variable
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	a = 0;
+	b = 5;
+	do {
+		printf("%d\n", a);
+		a++;
+	} while (a < b);
+	
+	return (0);
+}
