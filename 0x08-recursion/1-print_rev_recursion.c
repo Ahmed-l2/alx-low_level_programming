1c4d10755cb4427d066f53f826462cbebeb03b2a
Ahmed <ahmed_l2@yahoo.com>
2023-09-28 11:02:55 -0700

    function that prints string in reverse using recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -4,12 +4,12 @@
  * _print_rev_recursion - prints a string in reverse using recursion
  * @s: given string
  */
- 
 void _print_rev_recursion(char *s)
 {
 	if (s == NULL || *s == '\0')
 	{
 		_putchar('\n');
+		return;
 	}
 	_print_rev_recursion(s + 1);
 	putchar(*s);
