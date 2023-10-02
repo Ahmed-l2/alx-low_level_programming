9aeca8d0608b6cd305aa3725dcb348a96531f235
Ahmed <ahmed_l2@yahoo.com>
2023-10-02 11:16:53 -0700

    prints all arguments it receives.

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry pooint
+ * Description: program that prints all arguments it receives.
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: always 0 (sucess)
+ */
+
+int main (int argc, char *argv[])
+{
+	int count;
+
+	if(argc > 1)
+	{
+		for (count = 0; count < argc; count++)
+		{
+			printf("%s\n", argv[count]);
+		}
+	}
+	return 0;
+}
