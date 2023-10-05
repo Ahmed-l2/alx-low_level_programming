f14a1acc855650e2b7ef074180ad6bb0b51df5d0
Ahmed <ahmed_l2@yahoo.com>
2023-10-04 18:06:56 -0700

    function that splits a string into words.

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -7,37 +7,35 @@
  */
 char **strtow(char *str)
 {
+	int num_words = 0;
+	int i, word_index, start, word_length, j;
 
-	int num_words = 0, word_index = 0;
-	int start, end, word_length, j;
-	int i = 0;
-	char **ptr;
+	if (!str || !*str)
+		return (NULL);
+	for (i = 0; str[i]; i++)
+		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
+			num_words++;
+	if (num_words == 0)
+		return (NULL);
+	char **ptr = (char **)malloc((num_words + 1) * sizeof(char *));
 
+	if (!ptr)
+		return (NULL);
+	i = 0;
+	word_index = 0;
 	while (str[i])
+	{
 		while (str[i] == ' ')
 			i++;
 		if (str[i])
-			num_words++;
-			while (str[i] && str[i] != ' ')
-				i++;
-	if (num_words == 0)
-		return (NULL);
-	ptr = (char **)malloc((num_words + 1) * sizeof(char *));
-	if (ptr == NULL)
-		return (NULL);
-	i = 0;
-	while (str[i])
-		while (str[i] != ' ')
-			i++;
-		if (str[i])
+		{
 			start = i;
 			while (str[i] && str[i] != ' ')
 				i++;
-			end = i - 1;
-			word_length = end - start + 1;
+			word_length = i - start;
 			ptr[word_index] = (char *)malloc((word_length + 1) * sizeof(char));
-			if (ptr[word_index] == NULL)
-				for (j = 0; j < word_index; j++)
+			if (!ptr[word_index])
+				for (j = 0; j <= word_index; j++)
 					free(ptr[j]);
 				free(ptr);
 				return (NULL);
@@ -45,6 +43,8 @@ char **strtow(char *str)
 				ptr[word_index][j] = str[start + j];
 			ptr[word_index][word_length] = '\0';
 			word_index++;
+		}
+	}
 	ptr[num_words] = NULL;
 	return (ptr);
 }
