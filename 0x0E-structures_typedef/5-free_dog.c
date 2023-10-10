feb0c2a180b7e99c38f014b663070929a9506e80
Ahmed <ahmed_l2@yahoo.com>
2023-10-10 11:58:30 -0700

    function that frees dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+
+/**
+ * free_dog - function that frees dogs
+ * @d: dog to free
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->age);
+	free(d->owner);
+	free(d);
+}
