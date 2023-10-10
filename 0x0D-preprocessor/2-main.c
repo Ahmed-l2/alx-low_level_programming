fc8b4b80dd12e4d17616ace6fe54848c41bfd926
Ahmed <ahmed_l2@yahoo.com>
2023-10-10 15:42:57 -0700

    program that prints its name followed by a new line

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * Description: function that prints the name of the program
+ * Return: always 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
