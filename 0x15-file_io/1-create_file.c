f2a2e60dc862be3ecc56f5448efa7193f84fd433
Ahmed <ahmed-l2@yahoo.com>
2023-10-31 00:10:13 +0300

    function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,11 +9,13 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int f, i = 0;
+	int f;
+	ssize_t bytesWritten = 0, length = 0;
 
-	while (text_content[i] != '\0')
+
+	while (text_content[length] != '\0')
 	{
-		i++;
+		length;
 	}
 
 	if (!filename)
@@ -24,15 +26,11 @@ int create_file(const char *filename, char *text_content)
 	if (f == -1)
 		return (-1);
 
-	if (text_content)
+	if (length)
 	{
-		if (write(f, text_content, i) == -1)
-		{
-			close(f);
-			return (-1);
-		}
+		write(f, text_content, length);
 	}
 
 	close(f);
-	return (1);
+	return (bytesWritten == length ? 1 : -1);
 }
