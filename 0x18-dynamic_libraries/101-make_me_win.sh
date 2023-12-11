0a0e5967f84215a915db931598b8f961c17d4744
Ahmed <ahmed_l2@yahoo.com>
2023-12-12 00:21:33 +0300

    3. Code injection: Win the Giga Millions!

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Ahmed-l2/alx-low_level_programming/raw/master/0x18-dynamic_libraries/gigam.so
-export LD_PRELOAD=/tmp/gigam.so
+wget -P /tmp https://github.com/Ahmed-l2/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgigam.so
+export LD_PRELOAD=/tmp/libgigam.so
