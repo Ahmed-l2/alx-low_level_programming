837f82f04f7528d3c450bf2507198afea3a225b8
Ahmed <ahmed_l2@yahoo.com>
2023-12-24 18:55:50 +0300

    3. >>> ht['betty'] = 'cool'

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,7 +11,7 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int i;
-	hash_note_t *new_node, current = NULL;
+	hash_node_t *new_node, current = NULL;
 
 	if (!ht || !key || !value)
 		return (0);
